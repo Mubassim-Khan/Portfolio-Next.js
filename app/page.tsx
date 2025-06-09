e32d6628499b67bc86b54ec6847021515d8d0708
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-06-09 12:05:41 +0000

    feat: add favicon & Metadata

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Banner from "@/components/Banner";
+import Skills from "@/components/Skills";
+import Certifications from "@/components/Certifications";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+
+export default function HomePage() {
+    return (
+        <>
+            <Banner />
+            <Skills />
+            <Projects />
+            <Certifications />
+            <Contact />
+        </>
+    )
+}
