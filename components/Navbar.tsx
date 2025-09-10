c10554c5529681c0dc53ed0c6fd6af0a0f598eea
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-09-10 08:59:05 +0000

    feat: change navbar bg color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
         : "translateY(-100%)";
       navRef.current.style.opacity = isNavVisible ? "1" : "0";
       navRef.current.style.transition =
-        "transform 0.3s ease, opacity 0.3s ease";
+        "transform 0.5s ease, opacity 0.5s ease";
     }
   }, [isNavVisible]);
 
@@ -57,7 +57,7 @@ const Navbar = () => {
     <div
       ref={navRef}
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
-      style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+      style={{ transition: "transform 0.5s ease, opacity 0.5s ease" }}
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between px-5">
